82cc904cd02ef863d8c42d0b49aecf726faa1e63
Ed Putans <edputans@gmail.com>
2018-11-06 14:07:54 +0000

    app works fully, but SSOT not confirmed

--- src/index.js
+++ src/index.js
@@ -1,40 +1,58 @@
 let localDoggos = ''
+const filterButton = document.querySelector('#good-dog-filter')
+const doginfo = document.querySelector('#dog-info')
+const pupperBar = document.querySelector('#dog-bar')
+
+
+const refreshDoggos = () => {
+    if (filterButton.innerText ==="Filter good dogs: ON!!!"){
+        pupperBar.innerHTML = ""
+        filteredPuppers = localDoggos.filter(dog => dog.isGoodDog)
+        addPuppersToPage(filteredPuppers)
+    }
+}
+
 
 const retrievePuppers = () => fetch('http://localhost:3000/pups')
     .then((response) => response.json())
     .then((responseJSON) => {
-       return localDoggos = responseJSON;
+    // localDoggos = responseJSON
+    return responseJSON
 })
 
 
-
 const addPupperToPage = pupper => {
-     
-    let goodness = pupper.isGoodDog === true? "Good boy" : "Bad boy"
-    const doginfo = document.querySelector('#dog-info')
-    const pupperBar = document.querySelector('#dog-bar')
-
     const pupperElement = document.createElement('div')
     pupperElement.innerHTML= `
         <span><br>${pupper.name}<br> </span>
     `
+
+
     pupperElement.addEventListener('click', event => {
         doginfo.innerHTML=
         `<div class="polaroid">
          <img src="${pupper.image}" alt="5 Terre" style="width:100%">
           <div class="container">
           <p>${pupper.name}</p>
-          <button id="good-boiness">${goodness}</button>
+          <button id="good-boiness">${pupper.isGoodDog === true? "Good dog" : "Bad dog"}</button>
         </div>
       </div>`
-    })
-    pupperBar.appendChild(pupperElement)
-    
-    // const button = document.querySelector('#good-boiness')
-    // button.addEventListener('click', event => {
-    //     goodness = pupper.isGoodDog === true? "Bad boy" : "Good boy"
-    // })
 
+     const butt = document.querySelector('#good-boiness')
+      butt.addEventListener('click', event => {
+        pupper.isGoodDog = pupper.isGoodDog === true? false : true
+        fetch(`http://localhost:3000/pups/${pupper.id}`, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(pupper)
+        }).then( doggoinfo=>  console.log(`doggo has been updated to ${pupper.isGoodDog}`)  )
+     
+        refreshDoggos()
+        butt.innerText = pupper.isGoodDog === true? "Good dog" : "Bad dog"
+      })
+    })
+
+    pupperBar.appendChild(pupperElement)
 }
 
 
@@ -43,5 +61,25 @@ const addPuppersToPage = pupperList => {
 }
 
 
-//run upon load
+
+filterButton.addEventListener("click", event => {
+    if (filterButton.innerText ==='Filter good dogs: OFF'){
+        filterButton.innerText = "Filter good dogs: ON!!!"
+        pupperBar.innerHTML = ""
+        filteredPuppers = localDoggos.filter(dog => dog.isGoodDog)
+        addPuppersToPage(filteredPuppers)
+    }
+    else if (filterButton.innerText==='Filter good dogs: ON!!!'){
+        filterButton.innerText = 'Filter good dogs: OFF'
+        pupperBar.innerHTML=''
+        addPuppersToPage(localDoggos)
+    }
+
+})
+
+
+
+// run upon loading page
 retrievePuppers().then(dogs => addPuppersToPage(dogs))
+
+
